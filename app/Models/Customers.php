36f7a0a70444f42b86d4876f3bb21ec96328c1f7
AnshuAlawa <alawapappu@gmail.com>
2023-06-26 12:28:51 +0530

    model

--- app/Models/Customers.php
+++ app/Models/Customers.php
@@ -10,4 +10,15 @@ class Customers extends Model
     use HasFactory;
     protected $table = "customers";
     protected $primaryKey = 'customer_id';
+
+    public function setNameAttribute($value)  // Mutteter
+    {
+        $this->attributes['name'] = ucwords($value);
+    }
+
+    public function getDobAttribute($value) // Acceeser
+    {
+        return date("d-M-Y", strtotime($value));
+    }
+
 }
