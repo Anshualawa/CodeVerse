3f583e7f2dc03edb4dad97e6baa0c6b8da4a7de0
AnshuAlawa <alawapappu@gmail.com>
2023-06-30 17:30:47 +0530

    dummy data

--- app/Http/Controllers/home.php
+++ app/Http/Controllers/home.php
@@ -10,8 +10,16 @@ class home extends Controller
     public function home()
     {
         $content = php_content::all();
+        $dummyy_data = [
+            ['name' => 'alawa', 'email' => 'alawa@gmail.com', 'age' => 23],
+            ["name" => "Ram", "email" => "ram@gmail.com", "age" => 23],
+            ["name" => "Shyam", "email" => "shyam23@gmail.com", "age" => 28],
+            ["name" => "John", "email" => "john@gmail.com", "age" => 33],
+            ["name" => "Bob", "email" => "bob32@gmail.com", "age" => 41]
+        ];
         $data = compact('content');
+        $dummy_data=compact('dummyy_data');
         toast('php content open Success ', 'success');
-        return view('home')->with($data);
+        return view('home')->with($data,$dummy_data);
     }
 }
